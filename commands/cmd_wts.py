668cded2e3510013fff16e6ab6b29124bd4282c3
jimhsu <mail+github@jhpath.com>
2018-12-05 11:06:48 -0600

    WTS

--- commands/cmd_wts.py
+++ commands/cmd_wts.py
@@ -1,6 +1,7 @@
 import ast
 import discord
 from discord import Embed, Color
+from operator import itemgetter, attrgetter, methodcaller
 import STATICS
 
 
@@ -35,29 +36,31 @@ def ex(args, message, client, invoke, sender):
                         f.write("{}".format(neworder))
                         f.write("\n")
 
+                    buylist = []
+                    selllist = []
+
                     with open("orderlist.txt") as f:
-                        for line in f:
-                            entry = ast.literal_eval(line)
-                            print(entry[0])
-                            if entry[0] == 'Buy':
-                                sendstr = sendstr + "\n" + str(entry[1])[:-5] + " - " + str(
-                                    entry[2]) + ' ' + STATICS.CURRENCY + ' @ ' + str(entry[3]) + ' ' + STATICS.CURRENCY2
-                            else:
-                                continue
+                      for line in f:
+                        entry = ast.literal_eval(line)
+                        if entry[0] == 'Buy':
+                            buylist.append(entry)
+                        elif entry[0] == 'Sell':
+                            selllist.append(entry)
+                        else:
+                            continue
+
+                    buylist = sorted(buylist,key=itemgetter(3))
+                    selllist = sorted(selllist,key=itemgetter(3))
+
+                    for entry in buylist:
+                        sendstr = sendstr + "\n" + str(entry[1])[:-5] + " - " + str(entry[2]) + ' ' + STATICS.CURRENCY + ' @ ' + str(entry[3]) + ' ' + STATICS.CURRENCY2
 
                     sendstr = sendstr + "\n\n" + "|WTS|"
 
-                    with open("orderlist.txt") as f:
-                        for line in f:
-                            entry = ast.literal_eval(line)
-                            print(entry[0])
-                            if entry[0] == 'Sell':
-                                sendstr = sendstr + "\n" + str(entry[1])[:-5] + " - " + str(
-                                    entry[2]) + ' ' + STATICS.CURRENCY + ' @ ' + str(entry[3]) + ' ' + STATICS.CURRENCY2
-                            else:
-                                continue
-
-                        yield from client.send_message(message.channel, sendstr)
+                    for entry in selllist:
+                        sendstr = sendstr + "\n" + str(entry[1])[:-5] + " - " + str(entry[2]) + ' ' + STATICS.CURRENCY + ' @ ' + str(entry[3]) + ' ' + STATICS.CURRENCY2
+                        
+                    yield from client.send_message(message.channel, sendstr)
                 else:
                     yield from client.send_message(message.channel,
                                                    embed=Embed(color=Color.red(), description=(STATICS.HELPER)))
