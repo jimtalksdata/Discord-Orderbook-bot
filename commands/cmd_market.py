ddf03848e148afdb662ced949b83256fd1ca44bd
jimhsu <mail+github@jhpath.com>
2018-12-05 10:52:11 -0600

    Update cmd_market.py
    Add sorting

--- commands/cmd_market.py
+++ commands/cmd_market.py
@@ -4,28 +4,33 @@ from discord import Embed, Color
 import STATICS
 
 def ex(args, message, client, invoke, sender):
-
+    
     if len(args) is 0:
+        buylist = []
+        selllist = []
+        
+        with open("orderlist.txt") as f:
+          for line in f:
+            entry = ast.literal_eval(line)
+            if entry[0] == 'Buy':
+                buylist.append(entry)
+            elif entry[0] == 'Sell':
+                selllist.append(entry)
+            else:
+                continue
+        
+        buylist = sorted(buylist,key=itemgetter(3))
+        selllist = sorted(selllist,key=itemgetter(3))
+                    
         sendstr = """-\n:fire: **ORDERBOOK** :fire:\n\n|WTB|"""
 
-        with open("orderlist.txt") as f:
-            for line in f:
-                entry = ast.literal_eval(line)
-                print(entry[0])
-                if entry[0] == 'Buy':
-                    sendstr= sendstr + "\n" + str(entry[1])[:-5] + " - " + str(entry[2]) + ' ' + STATICS.CURRENCY + ' @ ' + str(entry[3]) + ' ' + STATICS.CURRENCY2
-                else:
-                    continue
+        for entry in buylist:
+            sendstr = sendstr + "\n" + str(entry[1])[:-5] + " - " + str(entry[2]) + ' ' + CURRENCY + ' @ ' + str(entry[3]) + ' ' + CURRENCY2
 
         sendstr = sendstr + "\n\n" + "|WTS|"
 
-        with open("orderlist.txt") as f:
-            for line in f:
-                entry = ast.literal_eval(line)
-                if entry[0] == 'Sell':
-                    sendstr = sendstr + "\n" + str(entry[1])[:-5] + " - " + str(entry[2]) + ' ' + STATICS.CURRENCY + ' @ ' + str(entry[3]) + ' ' + STATICS.CURRENCY2
-                else:
-                    continue
+        for entry in selllist:
+            sendstr = sendstr + "\n" + str(entry[1])[:-5] + " - " + str(entry[2]) + ' ' + CURRENCY + ' @ ' + str(entry[3]) + ' ' + CURRENCY2
 
         yield from client.send_message(message.channel, sendstr)
     else:
